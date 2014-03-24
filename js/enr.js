f3571fe5d4e2648613884ab602b0762afca6bcf1
Paul Lamere <Paul.Lamere@gmail.com>
2014-03-24 07:28:01 -0400

    Added playlist save, throbber,gen playlist on load

--- js/enr.js
+++ js/enr.js
@@ -1,6 +1,6 @@
 /**
- *      by @plamere             
- *		https://github.com/plamere/echo-nest-radio
+ *   by @plamere             
+ *   https://github.com/plamere/echo-nest-radio
  */
 
 $.ajaxSetup({ cache: false, traditional:true });
@@ -13,11 +13,15 @@ var numTracks = 40;
 
 require([
     '$api/models',
-    '$views/list#List'], 
+    '$views/list#List',
+    '$views/throbber#Throbber'], 
 
-    function(models, List) {
+    function(models, List, Throbber) {
         var player = models.player;
         var curTrack = null;
+        var curSongs = null;
+        var throbber = null;
+        var curPlaylistTrack = null;
 
         function updateStatus(track) {
             if (track) {
@@ -30,18 +34,12 @@ require([
             updateStatus(p.data.track);
         });
 
-        $("#go").click(function() {
-            if (curTrack) {
-                createPlaylist(curTrack);
-            }
-        });
-
-        models.player.load('track').done(function(p) {
-            updateStatus(p.track);
-        });
-
         function showPlaylist(songs) {
+            $("#playlist").show();
             if(songs){
+                $("#cur-playlist").text("Echo Nest Radio for " +
+                    curPlaylistTrack.name + " by " + curPlaylistTrack.artists[0].name);
+                curSongs = songs;
                 models.Playlist.createTemporary('temp_' + new Date().getTime()).done(function(tempPlaylist) {
                     tempPlaylist.load('tracks').done( 
                       function(playlist) {
@@ -62,8 +60,30 @@ require([
             }
             else{
                 $("#radio-tracks").empty();
-                $("#radio-tracks").append('No playlist found!');
+                info('No songs found!');
             }
+            throbber.hide();
+        }
+
+        function savePlaylist(songs) {
+            var name = curPlaylistTrack.artists[0].name + " radio";
+            models.Playlist.create(name).done(function(playlist) {
+                playlist.load('tracks').done( 
+                  function(playlist) {
+                    var tracks = [];
+                    $.each(songs, function(num,song){
+                        var uri = song.tracks[0].foreign_id;
+                        uri = uri.replace('-WW', '');
+                        tracks.push(models.Track.fromURI(uri));
+                    });
+                    var description = 'Echo Nest Radio based on the song ' 
+                        + curPlaylistTrack.name + " by " + curTrack.artists[0].name;
+                    playlist.setDescription(description);
+                    playlist.tracks.add.apply(playlist.tracks, tracks).done(function(tracks) {
+                        info("Playlist saved as '" + name + "'");
+                    });
+                });
+            });
         }
 
         function fetchSongRadioPlaylist(track) {
@@ -124,11 +144,40 @@ require([
         }
 
         function createPlaylist(track) {
+            info("");
+            throbber.show();
+            $("#playlist").hide();
+            curPlaylistTrack = track;
             fetchSongRadioPlaylist(track);
         }
 
         function info(msg) {
             $("#info").text(msg);
         }
+
+        throbber = Throbber.forElement(document.getElementById('info'));
+        throbber.hide();
+
+        $("#go").click(function() {
+            if (curTrack) {
+                createPlaylist(curTrack);
+            }
+        });
+
+        $("#save").click(function() {
+            if (curSongs) {
+                savePlaylist(curSongs);
+            } else {
+                info("Can't save an empty playlist");
+            }
+        });
+
+        models.player.load('track').done(function(p) {
+            updateStatus(p.track);
+            if (curTrack) {
+                createPlaylist(curTrack);
+            }
+        });
+
     }
 ); 
